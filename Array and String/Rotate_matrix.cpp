2611e382344a913f69a509637cf0665af6d38e53
Sourav Kumar Bhunia <93206401+souravbhunia07@users.noreply.github.com>
2023-08-01 22:06:26 +0530

    Update Rotate_matrix.cpp

--- Array and String/Rotate_matrix.cpp
+++ Array and String/Rotate_matrix.cpp
@@ -1,3 +1,4 @@
+// O(N^2)
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -47,3 +48,66 @@ int main() {
 
     return 0;
 }
+
+// O(N)
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void rotateMatrix(vector<vector<int>>& matrix) {
+    int n = matrix.size();
+
+    // Rotate layer by layer
+    for (int layer = 0; layer < n / 2; ++layer) {
+        int first = layer;
+        int last = n - 1 - layer;
+
+        for (int i = first; i < last; ++i) {
+            int offset = i - first;
+
+            // Save the top element
+            int top = matrix[first][i];
+
+            // Left -> Top
+            matrix[first][i] = matrix[last - offset][first];
+
+            // Bottom -> Left
+            matrix[last - offset][first] = matrix[last][last - offset];
+
+            // Right -> Bottom
+            matrix[last][last - offset] = matrix[i][last];
+
+            // Top -> Right
+            matrix[i][last] = top;
+        }
+    }
+}
+
+// Helper function to print the matrix
+void printMatrix(const vector<vector<int>>& matrix) {
+    for (const auto& row : matrix) {
+        for (int num : row) {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    vector<vector<int>> matrix = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    cout << "Original Matrix:" << endl;
+    printMatrix(matrix);
+
+    rotateMatrix(matrix);
+
+    cout << "Rotated Matrix:" << endl;
+    printMatrix(matrix);
+
+    return 0;
+}
+
