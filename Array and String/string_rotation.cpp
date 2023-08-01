d6f23d7e1629500628506d53d358ace24641a393
Sourav Kumar Bhunia <93206401+souravbhunia07@users.noreply.github.com>
2023-08-02 00:17:23 +0530

    Create string_rotation.cpp

---
+++ Array and String/string_rotation.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isSubstring(const string& str, const string& substr) {
+    if(str.find(substr))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool isRotation(const string& s1, const string& s2) {
+    if (s1.length() != s2.length()) {
+        return false; // Strings of different lengths cannot be rotations of each other
+    }
+
+    string concatenated = s1 + s1;
+    return isSubstring(concatenated, s2);
+}
+
+int main() {
+    string s1 = "waterbottle";
+    string s2 = "erbottlewat";
+
+    if (isRotation(s1, s2)) {
+        cout << s2 << " is a rotation of " << s1 << endl;
+    } else {
+        cout << s2 << " is not a rotation of " << s1 << endl;
+    }
+
+    return 0;
+}
