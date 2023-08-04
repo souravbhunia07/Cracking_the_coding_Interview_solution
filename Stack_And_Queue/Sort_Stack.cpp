3a5508871b49c20d9be50994b5ed180aed850916
Sourav Kumar Bhunia <93206401+souravbhunia07@users.noreply.github.com>
2023-08-04 13:06:46 +0530

    Update Sort_Stack.cpp

--- Stack_And_Queue/Sort_Stack.cpp
+++ Stack_And_Queue/Sort_Stack.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stack>
 
+// O(N^2)
 // Function to sort a stack in ascending order
 void sortStack(std::stack<int>& originalStack) {
     // Create an additional temporary stack to store elements in sorted order
@@ -30,6 +31,54 @@ void sortStack(std::stack<int>& originalStack) {
     }
 }
 
+// O(NlogN)
+void merge(std::stack<int>& left, std::stack<int>& right, std::stack<int>& result) {
+    while (!left.empty() && !right.empty()) {
+        if (left.top() < right.top()) {
+            result.push(left.top());
+            left.pop();
+        } else {
+            result.push(right.top());
+            right.pop();
+        }
+    }
+
+    while (!left.empty()) {
+        result.push(left.top());
+        left.pop();
+    }
+
+    while (!right.empty()) {
+        result.push(right.top());
+        right.pop();
+    }
+}
+
+void mergeSort(std::stack<int>& inputStack) {
+    if (inputStack.size() <= 1) {
+        return;
+    }
+
+    std::stack<int> left, right;
+    int size = inputStack.size();
+    int middle = size / 2;
+
+    for (int i = 0; i < middle; ++i) {
+        left.push(inputStack.top());
+        inputStack.pop();
+    }
+
+    for (int i = middle; i < size; ++i) {
+        right.push(inputStack.top());
+        inputStack.pop();
+    }
+
+    mergeSort(left);
+    mergeSort(right);
+
+    merge(left, right, inputStack);
+}
+
 int main() {
     std::stack<int> myStack;
     myStack.push(5);
@@ -39,19 +88,14 @@ int main() {
     myStack.push(3);
 
     std::cout << "Original Stack: ";
-    while (!myStack.empty()) {
-        std::cout << myStack.top() << " ";
-        myStack.pop();
+    std::stack<int> printStack = myStack;
+    while (!printStack.empty()) {
+        std::cout << printStack.top() << " ";
+        printStack.pop();
     }
     std::cout << std::endl;
 
-    myStack.push(5);
-    myStack.push(2);
-    myStack.push(8);
-    myStack.push(1);
-    myStack.push(3);
-
-    sortStack(myStack);
+    mergeSort(myStack);
 
     std::cout << "Sorted Stack: ";
     while (!myStack.empty()) {
