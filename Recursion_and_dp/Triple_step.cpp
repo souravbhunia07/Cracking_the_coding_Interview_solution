3817b674e20d1febf8ddfc330bed30b2274d6d21
Sourav Kumar Bhunia <93206401+souravbhunia07@users.noreply.github.com>
2023-08-21 20:14:24 +0530

    Create Triple_step.cpp

---
+++ Recursion_and_dp/Triple_step.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+int countWaysToClimbStairs(int n, std::vector<int>& dp) {
+    if (n <= 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    } else if (dp[n] > -1) {
+        return dp[n];
+    } else {
+        dp[n] = countWaysToClimbStairs(n - 1, dp) + countWaysToClimbStairs(n - 2, dp) + countWaysToClimbStairs(n - 3, dp);
+        return dp[n];
+    }
+}
+
+int main() {
+    int n;
+    std::cout << "Enter the number of steps: ";
+    std::cin >> n;
+
+    std::vector<int> dp(n + 1, -1);
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 4;
+    int ways = countWaysToClimbStairs(n, dp);
+    std::cout << "Number of possible ways: " << ways << std::endl;
+
+    return 0;
+}
